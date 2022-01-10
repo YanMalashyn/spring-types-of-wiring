77ab6e89ff190b377a40b18aa51d63dc9e2c29d4
Yan.Malashyn <lamantinspb1@gmail.com>
2022-01-10 13:49:47 +0300

    remaster

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,15 +1,10 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class Egg6 {
 
     private Needle7 needle7;
 
-    @Autowired
-    public void setNeedle7(Needle7 needle7){
+    public Egg6(Needle7 needle7) {
         this.needle7 = needle7;
     }
 
